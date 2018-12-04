fec00ee449d8d270838e66db1b5fe0b459a4f1f7
Muslim Guseinov <gusejnov1999@list.ru>
2018-12-04 22:21:33 +0300

    feature(Bootstrap): up apollo server, adding prisma config

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import * as Koa from 'koa';
+import * as staticMiddleware from 'koa-static';
+import { Prisma } from 'prisma-binding';
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-koa';
+import { createServer } from 'http';
+import { importSchema } from 'graphql-import';
+import { graphqlUploadKoa } from 'graphql-upload';
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = importSchema('./src/schema.graphql');
+const schema = makeExecutableSchema({
+	typeDefs,
+});
+
+const server = new ApolloServer({
+	schema,
+	uploads: false,
+	context: (req: any) => ({
+		...req,
+		db: new Prisma({
+			typeDefs: './src/generated/prisma.graphql',
+			endpoint: process.env.PRISMA_ENDPOINT,
+			debug: true,
+			secret: process.env.PRISMA_SECRET,
+		}),
+	}),
+});
+
+const app = new Koa();
+
+app.use(
+	graphqlUploadKoa({
+		maxFileSize: 10000000,
+		maxFiles: 20,
+	})
+);
+
+app.use(staticMiddleware('/images'));
+
+server.applyMiddleware({
+	app,
+});
+
+const httpServer = createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(
+	{
+		port: PORT,
+	},
+	() => {
+		console.log(
+			`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+		);
+		console.log(
+			`🚀 Subscriptions ready at ws://localhost:${PORT}${
+				server.subscriptionsPath
+			}`
+		);
+	}
+);
