e0a50d3336af0f20f734e7048806ca0648c25b7f
Muslim Guseinov <gusejnov1999@list.ru>
2018-12-05 01:14:43 +0300

    create(Query): create post query

---
+++ src/resolvers/Query.ts
@@ -0,0 +1,29 @@
+import { getUserId, Context } from '../utils/token';
+
+export const Query = {
+	feed(parent: any, args: any, ctx: Context, info: any) {
+		return ctx.db.query.posts({ where: { isPublished: true } }, info);
+	},
+
+	drafts(parent: any, args: any, ctx: Context, info: any) {
+		const id = getUserId(ctx);
+
+		const where = {
+			isPublished: false,
+			author: {
+				id,
+			},
+		};
+
+		return ctx.db.query.posts({ where }, info);
+	},
+
+	post(parent: any, { id }: { id: number }, ctx: Context, info: any) {
+		return ctx.db.query.post({ where: { id } }, info);
+	},
+
+	me(parent: any, args: any, ctx: Context, info: any) {
+		const id = getUserId(ctx);
+		return ctx.db.query.user({ where: { id } }, info);
+	},
+};
